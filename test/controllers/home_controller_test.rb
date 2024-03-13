f8495afb3eb39e6cfa8c89a16529a86058aa0d3d
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-12 22:19:26 -0300

    feat: define private actions in controllers

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'should get index' do
+    sign_in users(:john_doe)
+
     get root_url
 
     assert_response :success
