1682c9e02ede86316565f075dc775e389d5e1364
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-15 21:34:04 -0300

    fix: adjust cpf validator

--- app/validators/cpf_validator.rb
+++ app/validators/cpf_validator.rb
@@ -2,7 +2,7 @@
 
 class CpfValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if CPF.valid? value
+    return if CPF.valid?(value) && /\A\d+\z/.match?(value.to_s)
 
     error_options = options.except(:allow_blank, :allow_nil).merge!(value:)
     record.errors.add attribute, :invalid, **error_options
