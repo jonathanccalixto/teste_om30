239d49b58aa7dcf501584d33fdc5a85af644763a
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 08:44:12 -0300

    feat: apply inputmask to masked inputs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,14 @@ import "controllers"
 import "bootstrap"
 import 'vanillajs-datepicker'
 import 'vanillajs-datepicker-pt-BR'
+import 'inputmask'
 
 document.addEventListener('turbo:load', () => {
   document.querySelectorAll('input.date').forEach((field) => {
     new Datepicker(field, { language: 'pt-BR' });
+  });
+
+  document.querySelectorAll('input[data-mask]').forEach((field) => {
+    Inputmask(field.dataset.mask).mask(field);
   });
 });
