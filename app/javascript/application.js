3f3ff7a7688e0df64b0ab69536cad8cb7776d9b2
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 07:37:43 -0300

    feat: identify date field to all pages for apply datepicker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,10 +5,7 @@ import "bootstrap"
 import 'vanillajs-datepicker'
 
 document.addEventListener('turbo:load', () => {
-  const form = document.querySelector('.new.municipes,.edit.municipes,.create.municipes,.update.municipes')
-
-  if(!form) return
-
-  const dateField = document.querySelector('input.date');
-  const datepicker = new Datepicker(dateField);
+  document.querySelectorAll('input.date').forEach((dateField) => {
+    new Datepicker(dateField);
+  });
 });
