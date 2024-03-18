0ae7546195e8c3ea9281bc3e7be7ef45bc11de6e
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 07:21:38 -0300

    feat: add vanillajs-datepicker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import 'vanillajs-datepicker'
+
+document.addEventListener('turbo:load', () => {
+  const form = document.querySelector('.new.municipes,.edit.municipes,.create.municipes,.update.municipes')
+
+  if(!form) return
+
+  const dateField = document.querySelector('input.date');
+  const datepicker = new Datepicker(dateField);
+});
