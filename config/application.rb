42b388f7c8b980f5b02b692ebdf5a46192b2ddce
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 00:50:50 -0300

    chore: configure pt-BR as default translation

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module TestOm30
     # config.time_zone = "Central Time (US & Canada)"
     config.time_zone = 'Brasilia'
 
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
     # config.eager_load_paths << Rails.root.join("extras")
     config.eager_load_paths << Rails.root.join('/app/**/concerns')
   end
