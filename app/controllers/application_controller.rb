b59b5441cfb6c8181b3b5e7b174033be4befa28e
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 09:12:39 -0300

    chore: define adorne helper to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def adorne(record)
+    @adornes ||= {}
+    @adornes[record] ||= begin
+      "#{record.class}Decorator".constantize.new record
+    rescue StandardError
+      ApplicationDecorator.new record
+    end
+  end
+  helper_method :adorne
 end
