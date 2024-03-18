0775c9a2f8c696e3d470de5949a12d9cf2391960
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2024-03-18 08:43:37 -0300

    chore: import inputmask

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,4 @@ pin 'bootstrap' # @5.3.3
 pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js' # @2.11.8
 pin 'vanillajs-datepicker', to: 'https://unpkg.com/vanillajs-datepicker@1.3.4/dist/js/datepicker-full.min.js' # @1.3.4
 pin 'vanillajs-datepicker-pt-BR', to: 'https://unpkg.com/vanillajs-datepicker@1.3.4/dist/js/locales/pt-BR.js' # @1.3.4
+pin 'inputmask' # @5.0.8
